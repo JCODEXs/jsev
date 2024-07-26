9508aada0e635b95ac3422be09fe4366ad257359
JCODEXs <juansebastianescobar.vega@gmail.com>
2024-07-26 09:30:40 -0500

    expandingLinkArea

--- src/app/page.js
+++ src/app/page.js
@@ -848,17 +848,17 @@ function about() {
                   <div>
                     <ul className="group/list">
                       <li className="mb-12">
-                        <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-                          <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
-                          <div className="z-10 sm:order-2 sm:col-span-6">
-                            <h3>
-                              <a
-                                className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
-                                href="https://delicipes.mettalecode.site/"
-                                target="_blank"
-                                rel="noreferrer"
-                                aria-label="Build a Spotify Connected App"
-                              >
+                        <a
+                          className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                          href="https://delicipes.mettalecode.site/"
+                          target="_blank"
+                          rel="noreferrer"
+                          aria-label="Build a Spotify Connected App"
+                        >
+                          <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                            <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                            <div className="z-10 sm:order-2 sm:col-span-6">
+                              <h3>
                                 <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
                                 <span>
                                   Delicipes Plan. Shop. Eat.
@@ -879,72 +879,72 @@ function about() {
                                     </svg>
                                   </span>
                                 </span>
-                              </a>
-                            </h3>
-                            <p className="mt-2 text-sm leading-normal">
-                              meal planning with detailed recipes, cost
-                              calculations, and easy-to-use planners. Generate
-                              grocery lists based on your weekly meal plan and
-                              stay within budget effortlessly
-                            </p>
-                            <ul
-                              className="mt-2 flex flex-wrap"
-                              aria-label="Technologies used:"
-                            >
-                              <li className="mr-1.5 mt-2">
-                                <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                  S3
-                                </div>
-                              </li>
-                              <li className="mr-1.5 mt-2">
-                                <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                  NextJs
-                                </div>
-                              </li>
-                              <li className="mr-1.5 mt-2">
-                                <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                  MongoDB
-                                </div>
-                              </li>
-                              <li className="mr-1.5 mt-2">
-                                <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                  Tailwind
-                                </div>
-                              </li>
-                              <li className="mr-1.5 mt-2">
-                                <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                  Shadcn
-                                </div>
-                              </li>
-                              <li className="mr-1.5 mt-2">
-                                <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                  Clerk
-                                </div>
-                              </li>
-                              <li className="mr-1.5 mt-2">
-                                <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                  Docker
-                                </div>
-                              </li>
-                              <li className="mr-1.5 mt-2">
-                                <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                  Zustand
-                                </div>
-                              </li>
-                            </ul>
+                              </h3>
+                              <p className="mt-2 text-sm leading-normal">
+                                meal planning with detailed recipes, cost
+                                calculations, and easy-to-use planners. Generate
+                                grocery lists based on your weekly meal plan and
+                                stay within budget effortlessly
+                              </p>
+                              <ul
+                                className="mt-2 flex flex-wrap"
+                                aria-label="Technologies used:"
+                              >
+                                <li className="mr-1.5 mt-2">
+                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                    S3
+                                  </div>
+                                </li>
+                                <li className="mr-1.5 mt-2">
+                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                    NextJs
+                                  </div>
+                                </li>
+                                <li className="mr-1.5 mt-2">
+                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                    MongoDB
+                                  </div>
+                                </li>
+                                <li className="mr-1.5 mt-2">
+                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                    Tailwind
+                                  </div>
+                                </li>
+                                <li className="mr-1.5 mt-2">
+                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                    Shadcn
+                                  </div>
+                                </li>
+                                <li className="mr-1.5 mt-2">
+                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                    Clerk
+                                  </div>
+                                </li>
+                                <li className="mr-1.5 mt-2">
+                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                    Docker
+                                  </div>
+                                </li>
+                                <li className="mr-1.5 mt-2">
+                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                    Zustand
+                                  </div>
+                                </li>
+                              </ul>
+                            </div>
+                            <img
+                              alt="Delicipes"
+                              loading="lazy"
+                              width="200"
+                              height="48"
+                              decoding="async"
+                              data-nimg="1"
+                              className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+                              style={{ color: "transparent" }}
+                              src="/Il_nostro_matrimonio (1).png"
+                            />
                           </div>
-                          <img
-                            alt="Delicipes"
-                            loading="lazy"
-                            width="200"
-                            height="48"
-                            decoding="async"
-                            data-nimg="1"
-                            className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
-                            style={{ color: "transparent" }}
-                            src="/Il_nostro_matrimonio (1).png"
-                          />
-                        </div>
+                        </a>
                       </li>
                       <li className="mb-12">
                         <a
@@ -1046,17 +1046,17 @@ function about() {
                         </a>
                       </li>
                       <li className="mb-12">
-                        <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-                          <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
-                          <div className="z-10 sm:order-2 sm:col-span-6">
-                            <h3>
-                              <a
-                                className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
-                                href="https://maderafi.mettalecode.site"
-                                target="_blank"
-                                rel="noreferrer"
-                                aria-label="maderafi"
-                              >
+                        <a
+                          className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                          href="https://maderafi.mettalecode.site"
+                          target="_blank"
+                          rel="noreferrer"
+                          aria-label="maderafi"
+                        >
+                          <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                            <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                            <div className="z-10 sm:order-2 sm:col-span-6">
+                              <h3>
                                 <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
                                 <span>
                                   Maderafina
@@ -1076,13 +1076,12 @@ function about() {
                                     </svg>
                                   </span>
                                 </span>
-                              </a>
-                            </h3>
-                            <p className="mt-2 text-sm leading-normal">
-                              Landing page used to show products and prices of
-                              wood forniture
-                            </p>
-                            {/* <a
+                              </h3>
+                              <p className="mt-2 text-sm leading-normal">
+                                Landing page used to show products and prices of
+                                wood forniture
+                              </p>
+                              {/* <a
                               className="relative mt-2 inline-flex items-center text-sm font-medium text-slate-300 hover:text-teal-300 focus-visible:text-teal-300"
                               href="https://maderafi.mettalecode.site"
                               target="_blank"
@@ -1101,40 +1100,41 @@ function about() {
                               </svg>
                               <span>Maderafina</span>
                             </a> */}
-                            <ul
-                              className="mt-2 flex flex-wrap"
-                              aria-label="Technologies used:"
-                            >
-                              <li className="mr-1.5 mt-2">
-                                <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                  ThreeJs
-                                </div>
-                              </li>
-                              <li className="mr-1.5 mt-2">
-                                <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                  NextJs
-                                </div>
-                              </li>
-                              <li className="mr-1.5 mt-2">
-                                <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                  Tailwind
-                                </div>
-                              </li>
-                            </ul>
-                          </div>
+                              <ul
+                                className="mt-2 flex flex-wrap"
+                                aria-label="Technologies used:"
+                              >
+                                <li className="mr-1.5 mt-2">
+                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                    ThreeJs
+                                  </div>
+                                </li>
+                                <li className="mr-1.5 mt-2">
+                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                    NextJs
+                                  </div>
+                                </li>
+                                <li className="mr-1.5 mt-2">
+                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                    Tailwind
+                                  </div>
+                                </li>
+                              </ul>
+                            </div>
 
-                          <img
-                            alt=""
-                            loading="lazy"
-                            width="200"
-                            height="48"
-                            decoding="async"
-                            data-nimg="1"
-                            className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
-                            style={{ color: "transparent" }}
-                            src="/maderafina.png"
-                          />
-                        </div>
+                            <img
+                              alt=""
+                              loading="lazy"
+                              width="200"
+                              height="48"
+                              decoding="async"
+                              data-nimg="1"
+                              className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+                              style={{ color: "transparent" }}
+                              src="/maderafina.png"
+                            />
+                          </div>
+                        </a>
                       </li>
                     </ul>
 
