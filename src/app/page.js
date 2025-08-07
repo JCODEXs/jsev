881522d7dbae8b399ce3b1f8907fcb1eefcf1a28
Juan Escobar <juansebastianescobar.vega@gmail.com>
2025-08-07 14:11:24 -0500

    spaceSAdjust

--- src/app/page.js
+++ src/app/page.js
@@ -41,7 +41,7 @@ function about() {
                     />
                   )}
 
-                  <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
+                  <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-3xl">
                     <a href="/">Juan S. Escobar</a>
                   </h1>
                   <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
@@ -50,25 +50,25 @@ function about() {
                   <div className="justify-center p-2 ">
                     <Image
                       src={"/JSEV.png"}
-                      height={270}
-                      width={200}
+                      height={180}
+                      width={150}
                       alt="juancho"
                       onLoad={handleImageLoad}
                       style={{
                         borderRadius: "5px",
-                        height: 270,
+                        height: 200,
                       }}
                     />
                   </div>
-                  <p className="mt-4 max-w-xs leading-normal">
+                  {/* <p className="mt-3 max-w-xs leading-normal">
                     I empower your ideas to soar, seamlessly blending the
                     virtual and physical realms.
-                  </p>
+                  </p> */}
                   <nav
                     className="nav hidden lg:block"
                     aria-label="In-page jump links"
                   >
-                    <ul className="mt-16 w-max">
+                    <ul className="mt-8 w-max">
                       <li>
                         <a
                           className="group flex items-center py-3 active"
@@ -888,6 +888,93 @@ function about() {
                   </div>
                   <div>
                     <ul className="group/list">
+                       <li className="mb-12">
+                        <a
+                          className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                          href="https://pvcalculator.jsescobar.pro/"
+                          target="_blank"
+                          rel="noreferrer"
+                          aria-label="colorMetta"
+                        >
+                          <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                            <div className="z-10 sm:order-2 sm:col-span-6">
+                              <h3>
+                                <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                                <span>
+                                  Metta Photovoltaics
+                                  <span className="inline-block">
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      viewBox="0 0 20 20"
+                                      fill="currentColor"
+                                      className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                                      aria-hidden="true"
+                                    >
+                                      <path
+                                        fillRule="evenodd"
+                                        d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                        clipRule="evenodd"
+                                      ></path>
+                                    </svg>
+                                  </span>
+                                </span>
+                              </h3>
+                              <p className="mt-2 text-sm leading-normal">
+                                Understand and optimize your energy consumption
+                                with our comprehensive suite of tools. Track
+                                energy usage of individual appliances. Visualize
+                                consumption patterns with interactive charts.
+                                Calculate potential savings with
+                                energy-efficient alternatives. Plan solar
+                                installations based on your actual energy needs
+                              </p>
+
+                              <ul
+                                className="mt-2 flex flex-wrap"
+                                aria-label="Technologies used:"
+                              >
+                                <li className="mr-1.5 mt-2">
+                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                    Once-ui
+                                  </div>
+                                </li>
+                                <li className="mr-1.5 mt-2">
+                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                    NextJs
+                                  </div>
+                                </li>
+                                <li className="mr-1.5 mt-2">
+                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                    Tailwind
+                                  </div>
+                                </li>
+                                <li className="mr-1.5 mt-2">
+                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                    Zod
+                                  </div>
+                                </li>
+                              </ul>
+                            </div>
+
+                            <Image
+                              alt=""
+                              loading="lazy"
+                              width="200"
+                              height="68"
+                              decoding="async"
+                              onLoad={handleImageLoad}
+                              data-nimg="1"
+                              className=" transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+                              style={{
+                                color: "transparent",
+                                height: "144px",
+                                width: "137px",
+                              }}
+                              src="/solarcalculator.png"
+                            />
+                          </div>
+                        </a>
+                      </li>
                       <li className="mb-12">
                         <a
                           className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
@@ -1186,93 +1273,7 @@ function about() {
                           </div>
                         </a>
                       </li>
-                      <li className="mb-12">
-                        <a
-                          className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
-                          href="https://pvcalculator.jsescobar.pro/"
-                          target="_blank"
-                          rel="noreferrer"
-                          aria-label="colorMetta"
-                        >
-                          <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-                            <div className="z-10 sm:order-2 sm:col-span-6">
-                              <h3>
-                                <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                                <span>
-                                  Metta Photovoltaics
-                                  <span className="inline-block">
-                                    <svg
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      viewBox="0 0 20 20"
-                                      fill="currentColor"
-                                      className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
-                                      aria-hidden="true"
-                                    >
-                                      <path
-                                        fillRule="evenodd"
-                                        d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                                        clipRule="evenodd"
-                                      ></path>
-                                    </svg>
-                                  </span>
-                                </span>
-                              </h3>
-                              <p className="mt-2 text-sm leading-normal">
-                                Understand and optimize your energy consumption
-                                with our comprehensive suite of tools. Track
-                                energy usage of individual appliances. Visualize
-                                consumption patterns with interactive charts.
-                                Calculate potential savings with
-                                energy-efficient alternatives. Plan solar
-                                installations based on your actual energy needs
-                              </p>
-
-                              <ul
-                                className="mt-2 flex flex-wrap"
-                                aria-label="Technologies used:"
-                              >
-                                <li className="mr-1.5 mt-2">
-                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                    Once-ui
-                                  </div>
-                                </li>
-                                <li className="mr-1.5 mt-2">
-                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                    NextJs
-                                  </div>
-                                </li>
-                                <li className="mr-1.5 mt-2">
-                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                    Tailwind
-                                  </div>
-                                </li>
-                                <li className="mr-1.5 mt-2">
-                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                    Zod
-                                  </div>
-                                </li>
-                              </ul>
-                            </div>
-
-                            <Image
-                              alt=""
-                              loading="lazy"
-                              width="200"
-                              height="68"
-                              decoding="async"
-                              onLoad={handleImageLoad}
-                              data-nimg="1"
-                              className=" transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
-                              style={{
-                                color: "transparent",
-                                height: "144px",
-                                width: "137px",
-                              }}
-                              src="/solarcalculator.png"
-                            />
-                          </div>
-                        </a>
-                      </li>
+                   
                     </ul>
 
                     <div className="mt-12 ">
