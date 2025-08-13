43a4857e09b334f5924b4b0c7481961431c24e1a
Juan Sebastian Escobar Vega <euler@MacBook-Air-de-Juan.local>
2025-08-13 13:22:53 -0500

    2025

--- src/app/page.js
+++ src/app/page.js
@@ -888,7 +888,7 @@ function about() {
                   </div>
                   <div>
                     <ul className="group/list">
-                       <li className="mb-12">
+                      <li className="mb-12">
                         <a
                           className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
                           href="https://pvcalculator.jsescobar.pro/"
@@ -971,103 +971,6 @@ function about() {
                                 width: "137px",
                               }}
                               src="/solarcalculator.png"
-                            />
-                          </div>
-                        </a>
-                      </li>
-                      <li className="mb-12">
-                        <a
-                          className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
-                          href="https://soccer.jsescobar.pro/"
-                          target="_blank"
-                          rel="noreferrer"
-                          aria-label="Build a Spotify Connected App"
-                        >
-                          <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-                            <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
-                            <div className="z-10 sm:order-2 sm:col-span-6">
-                              <h3>
-                                <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                                <span>
-                                  Soccer Ligue , Plan and register
-                                  <span className="inline-block">
-                                    {""}
-                                    <svg
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      viewBox="0 0 20 20"
-                                      fill="currentColor"
-                                      className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
-                                      aria-hidden="true"
-                                    >
-                                      <path
-                                        fillRule="evenodd"
-                                        d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                                        clipRule="evenodd"
-                                      ></path>
-                                    </svg>
-                                  </span>
-                                </span>
-                              </h3>
-                              <p className="mt-2 text-sm leading-normal">
-                                The data from you local soccer ligue presented
-                                in an clean structured way plan matches, manage
-                                teams players and referees ass well as you sport
-                                scenarios. register the results to acces team
-                                and player statistics.
-                              </p>
-                              <ul
-                                className="mt-2 flex flex-wrap"
-                                aria-label="Technologies used:"
-                              >
-                                <li className="mr-1.5 mt-2">
-                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                    S3
-                                  </div>
-                                </li>
-                                <li className="mr-1.5 mt-2">
-                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                    NextJs
-                                  </div>
-                                </li>
-                                <li className="mr-1.5 mt-2">
-                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                    Supabase
-                                  </div>
-                                </li>
-                                <li className="mr-1.5 mt-2">
-                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                    Tailwind
-                                  </div>
-                                </li>
-                                <li className="mr-1.5 mt-2">
-                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                    Shadcn
-                                  </div>
-                                </li>
-
-                                <li className="mr-1.5 mt-2">
-                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                    Docker
-                                  </div>
-                                </li>
-                                <li className="mr-1.5 mt-2">
-                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                    Zustand
-                                  </div>
-                                </li>
-                              </ul>
-                            </div>
-                            <Image
-                              alt="Delicipes"
-                              loading="lazy"
-                              width="200"
-                              height="68"
-                              decoding="async"
-                              onLoad={handleImageLoad}
-                              data-nimg="1"
-                              className=" transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
-                              style={{ color: "transparent", height: "110px" }}
-                              src="https://tqheuy3b23.ufs.sh/f/3nPJ2rcd2SU4JkdU33y7MuK10SFx69DosXtPACk5plhTc4Hv"
                             />
                           </div>
                         </a>
@@ -1176,10 +1079,10 @@ function about() {
                       <li className="mb-12">
                         <a
                           className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
-                          href="https://pensionescolombianos.co"
+                          href="https://soccer.jsescobar.pro/"
                           target="_blank"
                           rel="noreferrer"
-                          aria-label="Pensiones Colombianos"
+                          aria-label="Build a Spotify Connected App"
                         >
                           <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
                             <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
@@ -1187,8 +1090,9 @@ function about() {
                               <h3>
                                 <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
                                 <span>
-                                  Pensiones Colombianos{""}
+                                  Soccer Ligue , Plan and register
                                   <span className="inline-block">
+                                    {""}
                                     <svg
                                       xmlns="http://www.w3.org/2000/svg"
                                       viewBox="0 0 20 20"
@@ -1206,39 +1110,19 @@ function about() {
                                 </span>
                               </h3>
                               <p className="mt-2 text-sm leading-normal">
-                                Creating a web application to attract clients,
-                                collect their data, and host past legal
-                                sentences in PDF
+                                The data from you local soccer ligue presented
+                                in an clean structured way plan matches, manage
+                                teams players and referees ass well as you sport
+                                scenarios. register the results to acces team
+                                and player statistics.
                               </p>
-                              {/* <a
-                              className="relative mt-2 inline-flex items-center text-sm font-medium text-slate-300 hover:text-teal-300 focus-visible:text-teal-300"
-                              href="https://joinery.cap.jsescobar.pro/"
-                              target="_blank"
-                              rel="noreferrer"
-                              aria-label="580 stars on GitHub"
-                            >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="mr-1 h-3 w-3"
-                                aria-hidden="true"
-                              >
-                                <path
-                                  fillRule="evenodd"
-                                  d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
-                                  clipRule="evenodd"
-                                ></path>
-                              </svg>
-                              <span>Maderafina</span>
-                            </a> */}
                               <ul
                                 className="mt-2 flex flex-wrap"
                                 aria-label="Technologies used:"
                               >
                                 <li className="mr-1.5 mt-2">
                                   <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                    React
+                                    S3
                                   </div>
                                 </li>
                                 <li className="mr-1.5 mt-2">
@@ -1248,18 +1132,34 @@ function about() {
                                 </li>
                                 <li className="mr-1.5 mt-2">
                                   <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                                    MongoDB
+                                    Supabase
                                   </div>
                                 </li>
+                                <li className="mr-1.5 mt-2">
+                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                    Tailwind
+                                  </div>
+                                </li>
+                                <li className="mr-1.5 mt-2">
+                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                    Shadcn
+                                  </div>
+                                </li>
+
                                 <li className="mr-1.5 mt-2">
                                   <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
                                     Docker
                                   </div>
                                 </li>
+                                <li className="mr-1.5 mt-2">
+                                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                                    Zustand
+                                  </div>
+                                </li>
                               </ul>
                             </div>
                             <Image
-                              alt=""
+                              alt="socoer"
                               loading="lazy"
                               width="200"
                               height="68"
@@ -1267,13 +1167,12 @@ function about() {
                               onLoad={handleImageLoad}
                               data-nimg="1"
                               className=" transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
-                              style={{ color: "transparent" }}
-                              src="https://utfs.io/f/3nPJ2rcd2SU4zPqFakbEcRKy5wDrVlNb8MoU3TCxkun0O1aI"
+                              style={{ color: "transparent", height: "110px" }}
+                              src="https://tqheuy3b23.ufs.sh/f/3nPJ2rcd2SU4JkdU33y7MuK10SFx69DosXtPACk5plhTc4Hv"
                             />
                           </div>
                         </a>
                       </li>
-                   
                     </ul>
 
                     <div className="mt-12 ">
